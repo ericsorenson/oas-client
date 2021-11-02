ac59f88bcbdd2d79652ac7fd1d24a17a8f101381
Eric Sorenson <eric@sorenson.software>
2021-11-02 14:46:55 -0500

    add strict mode

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -2,4 +2,9 @@ import { UserApp } from './UserApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<UserApp />, window.document.querySelector('#app'));
+ReactDOM.render(
+  <React.StrictMode>
+    <UserApp />
+  </React.StrictMode>,
+  window.document.querySelector('#app'),
+);
